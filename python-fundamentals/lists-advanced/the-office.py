a31225805af25b6fcf6135b6eb2b61249d1bf5df
Antonio <73387462+antonio-vachev@users.noreply.github.com>
2022-06-18 23:22:39 +0300

    Add files via upload

---
+++ python-fundamentals/lists-advanced/the-office.py
@@ -0,0 +1,22 @@
+# You will receive two lines of input:
+# •	a list of employees' happiness as a string of numbers separated by a single space
+# •	a happiness improvement factor (single number).
+# Your task is to find out if the employees are generally happy in their office.
+# First, multiply each employee's happiness by the factor.
+# Then, print one of the following lines:
+# •	If half or more of the employees have happiness greater than or equal to the average:
+# "Score: {happy_count}/{total_count}. Employees are happy!"
+# •	Otherwise:
+# "Score: {happy_count}/{total_count}. Employees are not happy!"
+
+happiness = list(map(int, input().split(" ")))
+improvement = int(input())
+improved_happiness = list(map(lambda x: x * improvement, happiness))
+average_happiness = sum(improved_happiness) / len(improved_happiness)
+above_average = list(filter(lambda x: x >= average_happiness, improved_happiness))
+
+if len(above_average) >= len(improved_happiness)/2:
+    print(f"Score: {len(above_average)}/{len(improved_happiness)}. Employees are happy!")
+else:
+    print(f"Score: {len(above_average)}/{len(improved_happiness)}. Employees are not happy!")
+
